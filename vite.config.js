58f9cb07532ec428ac2d24797de28ba06c21dcba
michel_phung <phungvansyhb@gmail.com>
2024-11-24 22:40:03 +0700

    add https certificate for localcall

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
+import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(),basicSsl()],
 	define: {
 		global: 'window',
 		'process.env': {},
